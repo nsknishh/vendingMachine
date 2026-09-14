88b30dffef7be0e6fd406e19e9141eed22f696ec
128Shaheer <128Skhan@frhsd.com>
2026-09-14 10:39:34 -0400

    first

---
+++ src/vendingMachine.java
@@ -0,0 +1,19 @@
+public class vendingMachine {
+
+    public vendingMachine(int initialSnacks, int threshold) {
+        double snackCount = initialSnacks;
+        double minCapacity = threshold;
+    }
+
+    public static void main(String[] args) {
+
+        public boolean buySnack (int snackCount, int minCapacity) {
+            if (snackCount > 0) {
+                snackCount--;
+            }
+            return snackCount < minCapacity;
+
+
+        }
+    }
+}
