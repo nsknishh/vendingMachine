02422222ae95db0ed9656003ee7c0507684c70a2
nsknishh <128nkumar@frhsd.com>
2026-09-14 10:56:34 -0400

    create 3

--- src/vendingMachine.java
+++ src/vendingMachine.java
@@ -14,5 +14,13 @@ public class vendingMachine {
 
     public static void main(String[] args) {
 
+        vendingMachine vendingMachine = new vendingMachine(5, 3);
+
+        System.out.println("----------Starting the Vending Machine---------");
+
+        System.out.println("Bought snack 1. Restock Needed?" + vendingMachine.buySnack());
+        System.out.println("Bought snack 2. Restock Needed?" + vendingMachine.buySnack());
+        System.out.println("Bought snack 3. Restock Needed?" + vendingMachine.buySnack());
+        System.out.println("Bought snack 4. Restock Needed?" + vendingMachine.buySnack());
     }
 }
